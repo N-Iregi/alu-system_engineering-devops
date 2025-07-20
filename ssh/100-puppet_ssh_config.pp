9132e5d7813217cf64ce113191e9adba931cb5d4
N-Iregi <n.iregi@alustudent.com>
2025-07-20 01:32:51 +0000

    Create Puppet file

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,13 @@
-# This Puppet manifest ensures the SSH config file exists
-# It sets proper ownership, permissions, and secure SSH options
-
-file { '/home/vagrant/.ssh/config':
+# This Puppet manifest configures the global SSH client settings to use a specific private key and disable password authentication
+file { '/etc/ssh/ssh_config':
   ensure  => file,
-  owner   => 'vagrant',
-  group   => 'vagrant',
-  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
   content => @("EOC")
     Host *
       IdentityFile ~/.ssh/school
       PasswordAuthentication no
     | EOC
 }
+
